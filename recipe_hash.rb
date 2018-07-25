e6f914a9b3d3aaeaabb0067c6810c44717f6079a
LizaCotter <ecotter20@allendalecolumbia.org>
2018-07-25 20:10:03 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,5 +1,5 @@
 #code your solutions here
-perfect_10_recipe = {"almond flour" => "2"
+perfect_10_recipe = {
 
 #1. Create a hash called `perfect_10_recipe` that has the following data:
 #  - almond flour - 2 cups
